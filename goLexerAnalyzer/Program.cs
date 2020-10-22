5f251ec32b56e5a26f18c2dfcdaf6a3ad5a45c9e
Esteri <liriata666@gmail.com>
2020-10-22 19:11:35 +1000

    IntLiterals, Operators and Identifiers parsing

---
+++ goLexerAnalyzer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace goLexerAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Lexer lex = new Lexer();
+            lex.Parse("C://input.txt");
+        }
+    }
+}
